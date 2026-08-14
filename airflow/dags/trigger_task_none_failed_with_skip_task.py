d12298edbbcefdd22364d546e3cd887c45fe7a9e
Jrodo <trivero@fi.uba.ar>
2026-08-14 00:57:38 -0300

    add a task with the trigger none_failed to show succed even if a task in the pipeline is skipped

---
+++ airflow/dags/trigger_task_none_failed_with_skip_task.py
@@ -0,0 +1,49 @@
+from airflow.sdk import  task,dag,TriggerRule
+from datetime import datetime,timedelta
+from src.triggers import *
+default_args = {
+    'owner': 'jrodo',
+    'start_date': datetime(2024, 6, 1),
+    'retries': 0,
+    'retry_delay': timedelta(minutes=5),
+}
+
+@dag('trigger_task_none_failed_with_skip_task',
+    description="A dag that has a skipped task",
+    schedule='@once',
+    catchup=False,
+    tags=['workflow_api','trigger','decorators'],
+    default_args=default_args
+)
+def triggers_pipeline():
+    @task(task_id="start")
+    def start():
+        succeed()
+    
+    @task(task_id="Payment_succeed")
+    def payment():
+        skip()
+
+    @task(task_id="fraud_check")
+    def fraud_check():
+        succeed()
+
+    @task(task_id="inventory_check")
+    def inventory_check():
+        succeed()
+
+    @task(task_id="ship_order",trigger_rule=TriggerRule.NONE_FAILED)
+    def ship_order():
+        succeed()
+
+    start_task = start()
+    payment_task = payment()
+    fraud_check_task = fraud_check()
+    inventory_check_task = inventory_check()
+    ship_order_task = ship_order()
+
+    start_task >> [payment_task,fraud_check_task,inventory_check_task] >> ship_order_task
+
+triggers_pipeline()    
+
+     
